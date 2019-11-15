fccdd5e0384bc2c8a472ddddab91650a19c5e58b
Intsupco <Intsupco>
2019-11-14 21:50:17 -0500

    Consulta por filtro nivel

---
+++ proyectoFinal/Logica/ClsCentroMedico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Web.UI.WebControls;
+
+namespace Logica
+{
+    public class ClsCentroMedico
+    {
+        OrmHospitalesDataContext db = new OrmHospitalesDataContext();
+        public void consultarPorFiltro(ref GridView gdvTabla,string nivel)
+        {
+            var consulta = (from centroM in db.centro_medico where centroM.nivel_atencion == nivel select centroM).ToList();
+            gdvTabla.DataSource = consulta;
+            gdvTabla.DataBind();
+        }
+    }
+}
