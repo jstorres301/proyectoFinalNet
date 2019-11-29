97b12c7737268cbe0bef9e3713b99db0db675891
Stephi <Stephi@STEPHI-PC>
2019-11-29 09:02:39 -0500

    RegistrarCentroMedico falta

---
+++ proyectoFinal/proyectoFinal/registrarCentroMedico.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="registrarCentroMedico.aspx.cs" Inherits="proyectoFinal.registrarCentroMedico" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+   
+        <form id="form2" runat="server">
+        <asp:Label ID="lblIdcentromedico" runat="server" Text="Id Centro Medico"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtIdcentromedico" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblNombredelcentromedico" runat="server" Text="Nombre del centromedico"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtNombredelcentromedico" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblNivelatencion" runat="server" Text="Nivel de atencion"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtNivelatencion" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblHorarios" runat="server" Text="Horarios"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtHorarios" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblFoto" runat="server" Text="Foto"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtFoto" runat="server"></asp:TextBox>
+        <br />
+
+         <asp:Button ID="btnRegistrar" runat="server" Text="Registrar" OnClick="btnRegistrar_Click" />
+    </form>
+    
+</body>
+</html>
---
+++ proyectoFinal/proyectoFinal/registrarCentroMedico.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace proyectoFinal
+{
+
+
+    public partial class registrarCentroMedico
+    {
+
+        /// <summary>
+        /// Control form2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
+
+        /// <summary>
+        /// Control lblIdcentromedico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblIdcentromedico;
+
+        /// <summary>
+        /// Control txtIdcentromedico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtIdcentromedico;
+
+        /// <summary>
+        /// Control lblNombredelcentromedico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombredelcentromedico;
+
+        /// <summary>
+        /// Control txtNombredelcentromedico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombredelcentromedico;
+
+        /// <summary>
+        /// Control lblNivelatencion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNivelatencion;
+
+        /// <summary>
+        /// Control txtNivelatencion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNivelatencion;
+
+        /// <summary>
+        /// Control lblHorarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblHorarios;
+
+        /// <summary>
+        /// Control txtHorarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtHorarios;
+
+        /// <summary>
+        /// Control lblFoto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFoto;
+
+        /// <summary>
+        /// Control txtFoto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFoto;
+
+        /// <summary>
+        /// Control btnRegistrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegistrar;
+    }
+}
